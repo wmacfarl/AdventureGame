b2817f74d0bf67aa343b78b10181958772d0ac52
Will Macfarlane <wmacfarl@gmail.com>
2019-02-12 09:36:42 -0500

    Implimented asic animation and movement controls.

---
+++ Assets/Scripts/PlayerControlScript.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using static SimpleAnimator;
+using UnityEngine;
+
+public class PlayerControlScript : MonoBehaviour
+{
+    public float speed;
+    new Rigidbody2D rigidbody;
+
+    MovementFacing currentMovementFacing;
+    MovementState currentMovementState;
+
+    MovementFacing previousMovementFacing;
+    MovementState previousMovementState;
+
+    SimpleAnimator animator;
+
+   
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<SimpleAnimator>();
+    }
+
+    void Update()
+    {
+        bool canMove = true;
+        PerformActions(GetActionInput());
+        if (canMove)
+        {
+            PerformMovement(GetDirectionInput());
+        }
+    }
+
+
+    Vector2 GetDirectionInput()
+    {
+        float inputX = 0;
+        float inputY = 0;
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            inputX = -1;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            inputX = 1;
+        }
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            inputY = 1;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            inputY = -1;
+        }
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.S))
+        {
+            inputY = -1;
+        }
+
+        return new Vector2(inputX, inputY);
+    }
+
+    bool[] GetActionInput()
+    {
+        bool[] input = new bool[2];
+        input[0] = false;
+        input[1] = false;
+        if (Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.Period))
+        {
+            input[0] = true;
+        }
+        if (Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.Slash))
+        {
+            input[1] = true;
+        }
+        return input;
+    }
+
+    void PerformActions(bool[] input)
+    {
+        if (input[0])
+        {
+            PerformPrimaryAction();
+        }
+        else if (input[1])
+        {
+            PerformSecondaryAction();
+        }
+    }
+
+    public void PerformPrimaryAction()
+    {
+
+    }
+
+    public void PerformSecondaryAction()
+    {
+
+    }
+
+    void PerformMovement(Vector2 input)
+    {
+        Vector3 delta;
+        delta = new Vector3(speed * input.x, speed * input.y, 0);
+        rigidbody.MovePosition(transform.position + delta);
+
+        if (input.x != 0 || input.y != 0)
+        {
+
+            currentMovementState = MovementState.WALKING;
+
+            if (input.x > 0)
+            {
+                currentMovementFacing = MovementFacing.RIGHT;
+            }
+            else if (input.x < 0)
+            {
+                currentMovementFacing = MovementFacing.LEFT;
+
+            }
+            else if (input.y > 0)
+            {
+                currentMovementFacing = MovementFacing.UP;
+            }
+            else if (input.y < 0)
+            {
+                currentMovementFacing = MovementFacing.DOWN;
+            }
+        }
+        else
+        {
+            currentMovementState = MovementState.IDLING;
+        }
+
+        if (currentMovementFacing != previousMovementFacing || currentMovementState != previousMovementState)
+        {
+            animator.EnterAnimationState(currentMovementState, currentMovementFacing);
+        }
+
+        previousMovementState = currentMovementState;
+        previousMovementFacing = currentMovementFacing;
+    }
+
+}
---
+++ Assets/Scripts/SimpleAnimator.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleAnimator : MonoBehaviour
+{
+    public List<Sprite> walkLeftSprites;
+    public List<Sprite> walkUpSprites;
+    public List<Sprite> walkDownSprites;
+    public List<Sprite> walkRightSprites;
+
+    public List<Sprite> liftUpSprites;
+    public List<Sprite> liftDownSprites;
+    public List<Sprite> liftLeftSprites;
+    public List<Sprite> liftRightSprites;
+
+    public List<Sprite> idleLeftSprites;
+    public List<Sprite> idleUpSprites;
+    public List<Sprite> idleDownSprites;
+    public List<Sprite> idleRightSprites;
+
+    public float walkCycleDuration;
+    public float idleCycleDuration;
+    public float liftCycleDuration;
+
+    int currentAnimationFrame;
+
+    float walkFrameDuration;
+    float idleFrameDuration;
+    float liftFrameDuration;
+
+    int currentFrameIndex;
+    public List<Sprite> currentSpriteList;
+    float currentFrameDuration;
+    float timeAtLastFrame = 0;
+
+    SpriteRenderer sprite;
+
+    public enum MovementState
+    {
+        IDLING = 0,
+        WALKING = 1,
+        LIFTING = 2
+    }
+
+    public enum MovementFacing
+    {
+        UP = 0,
+        RIGHT = 1,
+        DOWN = 2,
+        LEFT = 3
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        EnterAnimationState(MovementState.IDLING, MovementFacing.RIGHT);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - timeAtLastFrame > currentFrameDuration)
+        {
+            timeAtLastFrame = Time.time;
+            currentFrameIndex++;
+            if(currentFrameIndex >= currentSpriteList.Count)
+            {
+                currentFrameIndex = 0;
+            }
+            sprite.sprite = currentSpriteList[currentFrameIndex];
+
+        }
+    }
+
+    float GetCycleDuration(MovementState movementState)
+    {
+        if (movementState == MovementState.IDLING)
+        {
+            return idleCycleDuration;
+        }
+        else if (movementState == MovementState.LIFTING)
+        {
+            return liftCycleDuration;
+        }
+        else if (movementState == MovementState.WALKING)
+        {
+            return walkCycleDuration;
+        }
+        else
+        {
+            Debug.Log("INVALID MOVEMENT STATE");
+            return -1;
+        }
+    }
+
+    public void EnterAnimationState(MovementState movementState, MovementFacing movementFacing)
+    {
+        currentSpriteList = GetAnimationSprites(movementState, movementFacing);
+        currentFrameDuration = GetCycleDuration(movementState) / currentSpriteList.Count;
+        currentFrameIndex = 0;
+        timeAtLastFrame = Time.time;
+    }
+
+    List<Sprite> GetAnimationSprites(MovementState movementState, MovementFacing movementFacing)
+    {
+        if (movementState == MovementState.IDLING && movementFacing == MovementFacing.LEFT)
+        {
+            return idleLeftSprites;
+        }
+        else if (movementState == MovementState.IDLING && movementFacing == MovementFacing.RIGHT)
+        {
+            return idleRightSprites;
+        }
+        else if (movementState == MovementState.IDLING && movementFacing == MovementFacing.UP)
+        {
+            return idleUpSprites;
+        }
+        else if (movementState == MovementState.IDLING && movementFacing == MovementFacing.DOWN)
+        {
+            return idleDownSprites;
+        }
+
+        else if (movementState == MovementState.WALKING && movementFacing == MovementFacing.LEFT)
+        {
+            return walkLeftSprites;
+        }
+        else if (movementState == MovementState.WALKING && movementFacing == MovementFacing.RIGHT)
+        {
+            return walkRightSprites;
+        }
+        else if (movementState == MovementState.WALKING && movementFacing == MovementFacing.UP)
+        {
+            return walkUpSprites;
+        }
+        else if (movementState == MovementState.WALKING && movementFacing == MovementFacing.DOWN)
+        {
+            return walkDownSprites;
+        }
+
+        else if (movementState == MovementState.WALKING && movementFacing == MovementFacing.LEFT)
+        {
+            return liftLeftSprites;
+        }
+        else if (movementState == MovementState.LIFTING && movementFacing == MovementFacing.RIGHT)
+        {
+            return liftRightSprites;
+        }
+        else if (movementState == MovementState.LIFTING && movementFacing == MovementFacing.UP)
+        {
+            return liftUpSprites;
+        }
+        else if (movementState == MovementState.LIFTING && movementFacing == MovementFacing.DOWN)
+        {
+            return liftDownSprites;
+        }
+        else
+        {
+            Debug.Log("NO ANIMATION STATE.  Facing = " + movementFacing +", State = " + movementState);
+            return null;
+        }
+    }
+
+    float GetAnimationDuration(List<Sprite> sprites) {
+
+        return -1f;
+    }
+
+
+
+}
